86301db1e389b035e509441e77b19de76f31bda1
Thomas Misilo <tmisilo@ksu.edu>
2025-02-05 17:12:19 +0000

    Pull frontend footer and brand from local repos

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -119,9 +119,9 @@ hooks.Filters.ENV_PATCHES.add_items(
             f"mfe-dockerfile-post-npm-install-{mfe}",
             """
            
-RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
+RUN npm install '@ex/frontend-component-footer@git+https://github.com/newliteraciesalliance/frontend-component-footer.git#sumac/indigo/nla'
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.2.2'
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.2.2'
+RUN npm install '@edx/brand@git+https://github.com/newliteraciesalliance/brand-openedx#sumac/indigo-nla'
 
 """,
         )
